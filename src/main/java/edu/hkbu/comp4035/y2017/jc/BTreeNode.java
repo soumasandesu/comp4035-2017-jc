dd5628240ca23caf0d09a59a2f537a4a0db4bedb
hkgsherlock <hkgsherlock@gmail.com>
2017-03-25 06:03:40 +0800

    sort out mind

--- src/main/java/edu/hkbu/comp4035/y2017/jc/BTreeNode.java
+++ src/main/java/edu/hkbu/comp4035/y2017/jc/BTreeNode.java
@@ -2,57 +2,67 @@ package edu.hkbu.comp4035.y2017.jc;
 
 import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.HashMap;
 
 // TODO: javadocs for BTreeNode<T>
-public class BTreeNode<VType> implements Serializable {
+public class BTreeNode<V> implements Serializable {
     private final BTree tree;
-    private final ArrayList<BTreeNode<VType>> leaves;
-    private final HashMap<Integer, VType> keys;
+    private final ArrayList<BTreeNode<V>> leaves;
+    private int key;
+    private V value;
+
+    // TODO: ctors
+
 
     public BTreeNode(BTree tree) {
         this.tree = tree;
+        this.key = 0;
+        this.value = null;
         this.leaves = new ArrayList<>(2 * t());
-        this.keys = new HashMap<>(2 * t() - 1);
     }
 
-    protected BTreeNode(BTree tree, ArrayList<BTreeNode<VType>> leaves, HashMap<Integer, VType> keys) {
-        this.tree = tree;
-        this.leaves = leaves;
-        this.keys = keys;
-    }
-
-    public BTreeNode<VType> getLeaveAt(int index) {
+    public BTreeNode<V> getLeaveAt(int index) {
         return this.leaves.get(index);
-    }
-
-    public VType getValueAtKey(int index) {
-        return this.keys.get(index);
-    }
-
-    public boolean hasKey(int index) {
-        return this.keys.containsKey(index);
     }
 
     public boolean isLeaf() {
         return leaves.size() > 0;
     }
 
+    public int getKey() {
+        return key;
+    }
+
+    public V getValue() {
+        return value;
+    }
+
+    public BTreeNode<V> getSubNode(int index) {
+        BTreeNode<V> ret = null;
+
+        for (BTreeNode<V> l : this.leaves) {
+            // TODO:
+        }
+
+        return ret;
+    }
+
+    /* tricky methods goes below */
+
     private int t() {
         return this.tree.getProperties().getDegree();
     }
 
-    protected BTreeNode<VType> father() {
+    protected BTreeNode<V> father() {
         throw new UnsupportedOperationException("not yet implemented.");
         // TODO: father();
     }
 
-    protected BTree<VType> predecessor() {
+    protected BTree<V> predecessor() {
         throw new UnsupportedOperationException("not yet implemented.");
         // TODO: get its idx from father(), if val < 0 even father unless most father is root, else get its idx - 1
     }
 
-    protected BTree<VType> successor() {
+    protected BTree<V> successor() {
         throw new UnsupportedOperationException("not yet implemented.");
         // TODO: get its idx from father(), if val > (2 * t) even father unless most father is root, else get its idx + 1
     }
